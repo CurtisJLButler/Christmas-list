b5aab6492001f561d04cd473c4af1b02712ab1da
Curtis Butler <curtisjlbutler@gmail.com>
2022-12-04 13:20:15 -0400

    Clean up And added the more info page

--- generateItem.py
+++ generateItem.py
@@ -3,8 +3,8 @@ f = open("items.html", "r")
 print(f.read())
 f = open("items.html", "a")
 
-
-href = input("Input the link for the item: ")
+price = input("Input the price of the Product")
+href = input("Input the link for the Product: ")
 src = input("Input the src for the image of the product: ")
 alt = input("Input an alt for the image: ")
 item = input("Input the name of the item: ")
@@ -16,6 +16,6 @@ if oO == "Y" or oO == "y":
 else:
     oO = ""
 
-f.write(f'\n \n<article class="listItem"> \n    <a href="{href}"><img style="float: left;" src="{src}" alt="{alt}"></a>\n    <h1>{item}</h1>\n   <p>{description}</p>{oO}\n</article>')
+f.write(f'\n \n    <p style="float: inline-start[];">{price}</p>\n    <article class="listItem"> \n    <a href="{href}" target="_blank"><img style="float: left;" src="{src}" alt="{alt}"></a>\n    <h1>{item}</h1>\n   <p>{description}</p>{oO}\n</article>')
 
 f.close()
